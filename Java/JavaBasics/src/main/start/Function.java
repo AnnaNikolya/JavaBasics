7adcf64a657619b3f29bb015018adb88c3178b26
AnnaNikolya <annenikolaevna@gmail.com>
2022-07-08 22:23:17 +0300

    add file function

--- Java/JavaBasics/src/main/start/Function.java
+++ Java/JavaBasics/src/main/start/Function.java
@@ -2,6 +2,27 @@ package main.start;
 
 public class Function {
     public static void main(String[] args) {
+//        double getAverageValue(int[] array)
+//        void printArrayToConsole(int[] array)
+//        void printCurrentTimeToConsole()
+//        double sum(double a, double b)
+
+        int [] array = {2,9,9,7};
+        printArrayToComsole(array);
+        double sum = 0.0;
+        sum = sumDouble(1.0, 2.0);
+        System.out.println(sum);
+
+    }
+
+    static double sumDouble (double a, double b) {
+        double sum = a+ b;
+        return sum;
+    }
+
+    static void printArrayToComsole(int [] arrayToPrint){
+        for (int i =0; i< arrayToPrint.length; i++)
+            System.out.println(arrayToPrint[i]);
 
     }
 }
